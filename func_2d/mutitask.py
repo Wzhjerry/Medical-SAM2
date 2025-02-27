03fb62b0271562a2b059f335b0e4048cefb2ebca
wangzhonghua <wangzhonghua@devairdoc.ns>
2025-02-27 23:31:05 +1100

    u

--- func_2d/mutitask.py
+++ func_2d/mutitask.py
@@ -160,7 +160,7 @@ class Multitask(Dataset):
                 # target_pseudo_lesion = Image.fromarray(np.uint8(label_pseudo_lesion))
 
                 mask = np.zeros_like(label)
-                mask[np.where(label > 1)] = 255
+                mask[np.where(label > 0)] = 255
                 # mask[label == 2] = 2
                 # mask[label_pseudo_vessel == 1] = 255
                 # mask[label_pseudo_lesion == 1] = 255
