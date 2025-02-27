a285d6e72a7bbeadd8f2b676c5a4bb491a538ee6
wangzhonghua <wangzhonghua@devairdoc.ns>
2025-02-28 10:10:19 +1100

    u

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 # python train_2d.py -net sam2 -exp_name vessel_new -vis 1 -sam_ckpt ./checkpoints/sam2_hiera_small.pt -sam_config sam2_hiera_s -image_size 1024 -out_size 1024 -b 4 -val_freq 1 -dataset vessel -data_path custom
 # python train_2d.py -net sam2 -exp_name od_new -vis 1 -sam_ckpt ./checkpoints/sam2_hiera_small.pt -sam_config sam2_hiera_s -image_size 1024 -out_size 1024 -b 4 -val_freq 1 -dataset od -data_path custom
-python train_2d.py -net sam2 -exp_name oc_new -vis 1 -sam_ckpt ./checkpoints/sam2_hiera_small.pt -sam_config sam2_hiera_s -image_size 1024 -out_size 1024 -b 4 -val_freq 1 -dataset oc -data_path custom
+# python train_2d.py -net sam2 -exp_name oc_new -vis 1 -sam_ckpt ./checkpoints/sam2_hiera_small.pt -sam_config sam2_hiera_s -image_size 1024 -out_size 1024 -b 4 -val_freq 1 -dataset oc -data_path custom
 python train_2d.py -net sam2 -exp_name ex_new -vis 1 -sam_ckpt ./checkpoints/sam2_hiera_small.pt -sam_config sam2_hiera_s -image_size 1024 -out_size 1024 -b 4 -val_freq 1 -dataset ex -data_path custom
 python train_2d.py -net sam2 -exp_name he_new -vis 1 -sam_ckpt ./checkpoints/sam2_hiera_small.pt -sam_config sam2_hiera_s -image_size 1024 -out_size 1024 -b 4 -val_freq 1 -dataset he -data_path custom
 
