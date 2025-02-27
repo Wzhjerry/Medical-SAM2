a67415bc3e74a674bc1bdb42185823836d641e3c
wangzhonghua <wangzhonghua@devairdoc.ns>
2025-02-27 18:26:35 +1100

    u

--- func_2d/utils.py
+++ func_2d/utils.py
@@ -416,7 +416,7 @@ def eval_seg(pred,true_mask_p,threshold):
             disc_mask = gt_vmask_p [:,0,:,:].squeeze(1).cpu().numpy().astype('int32')
     
             '''iou for numpy'''
-            eiou += iou(disc_pred,disc_mask)
+            eiou += jc(disc_pred,disc_mask)
 
             '''dice for torch'''
             edice += dice_coeff(vpred[:,0,:,:], gt_vmask_p[:,0,:,:]).item()
