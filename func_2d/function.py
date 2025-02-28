12c497184fa4938f7dc48c2dafab6e11189ee2b0
wangzhonghua <wangzhonghua@devairdoc.ns>
2025-02-28 11:57:11 +1100

    u

--- func_2d/function.py
+++ func_2d/function.py
@@ -272,7 +272,7 @@ def validation_sam(args, val_loader, epoch, net: nn.Module, clean_dir=True):
     net.eval()
 
     n_val = len(val_loader) 
-    threshold = (0.3, 0.3)
+    threshold = (0.5, 0.5)
     GPUdevice = torch.device('cuda:' + str(args.gpu_device))
 
     # init
